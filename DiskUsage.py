ef46069676ffe0bc2c3cdd9705f2b3d3bff93547
ArtemToporkov <toporkovartem259@gmail.com>
2024-07-26 18:15:21 +0500

    Dates into datetime objects

--- DiskUsage.py
+++ DiskUsage.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime
 
 
 class File:
@@ -6,15 +6,15 @@ class File:
         self.name = os.path.basename(path)
         self.location = path
         self.size = os.path.getsize(path)
-        self.creation_date = os.path.getctime(path)
-        self.change_date = os.path.getmtime(path)
+        self.creation_date = datetime.datetime.fromtimestamp(os.path.getctime(path))
+        self.change_date = datetime.datetime.fromtimestamp(os.path.getmtime(path))
         self.extension = os.path.splitext(path)[1]
         self.files = []
         self.folders = []
         self.previous = None
 
     def __repr__(self):
-        return (f'[{self.name} -- {self.location} -- {self.size} -- {self.change_date}]')
+        return (f'-- {self.name} -- {self.location} -- {self.size} -- {self.change_date} -- {self.creation_date}')
 
 
 DIRECTORY = r'C:\Users\topor\OneDrive\Рабочий стол\check'
@@ -33,4 +33,4 @@ def fill_disk_usage(path: str, current: File):
 
 
 fill_disk_usage(DIRECTORY, CURRENT)
-print(CURRENT.folders[0].files)
+print(CURRENT)
