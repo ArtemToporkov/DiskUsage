5d6ec28d9acef5a4ca1db4e37a6b7b4d3aacc04b
ArtemToporkov <toporkovartem259@gmail.com>
2024-08-01 17:00:58 +0500

    added chart

--- DiskUsage.py
+++ DiskUsage.py
@@ -1,46 +1,49 @@
 import os, datetime
 import xml.etree.ElementTree as ET
+from PyQt5.QtChart import QChartView
+import time
 
 
 class File:
     def __init__(self, path: str):
         self.name = os.path.basename(path)
         self.location = path
-        self.size = os.path.getsize(path)
+        self.size = 0 if os.path.isdir(path) else os.path.getsize(path)
         self.creation_date = datetime.datetime.fromtimestamp(os.path.getctime(path))
         self.change_date = datetime.datetime.fromtimestamp(os.path.getmtime(path))
         self.extension = os.path.splitext(path)[1]
         self.files = []
         self.folders = []
-        self.previous = None
-
-    def __repr__(self):
-        return (f'-- {self.name} -- {self.location} -- {self.size} -- {self.change_date} -- {self.creation_date}')
+        self.parents = []
 
 
-DIRECTORY = 'C:\\Users\\topor\\OneDrive\\Рабочий стол\\check'
-#DIRECTORY = 'D:\\'
+# DIRECTORY = 'C:\\Users\\topor\OneDrive\\Рабочий стол\\check'
+DIRECTORY = 'D:\\'
 CURRENT = File(DIRECTORY)
 
 
 def fill_disk_usage(path: str, current: File):
     try:
-        for file in (f for f in os.listdir(path) if not os.path.isdir(os.path.join(path, f))):
-            current.files.append(File(os.path.join(path, file)))
-    except:
+        for file in os.listdir(path):
+            if os.path.isdir(os.path.join(path, file)):
+                folder_instance = File(os.path.join(path, file))
+                current.folders.append(folder_instance)
+                folder_instance.parents.append(current)
+            else:
+                file_instance = File(os.path.join(path, file))
+                current.files.append(file_instance)
+                current.size += file_instance.size
+                for parent in current.parents:
+                    parent.size += file_instance.size
+    except PermissionError:
         pass
-
-    try:
-        for folder in (f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))):
-            current.folders.append(File(os.path.join(path, folder)))
-    except:
-        pass
-
     for folder in current.folders:
         fill_disk_usage(os.path.join(path, folder.name), folder)
 
 
 def build_tree():
+    start_time = time.time()
     fill_disk_usage(DIRECTORY, CURRENT)
+    print("--- %s seconds ---" % (time.time() - start_time))
     return CURRENT
 
--- Main.py
+++ Main.py
@@ -3,6 +3,24 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QTreeWidgetItem
 import DiskUsage
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
+from PyQt5.QtGui import QPainter, QPen, QColor
+from PyQt5.QtCore import Qt
+
+
+class QFileItem(QTreeWidgetItem):
+    def __init__(self, file: DiskUsage.File):
+        info = [
+            file.name,
+            str(file.size),
+            file.creation_date.strftime('%H:%M:%S %d.%m.%y'),
+            file.change_date.strftime('%H:%M:%S %d.%m.%y'),
+            str(file.extension)
+        ]
+        super().__init__(info)
+        self.file = file
 
 
 class MainWindow(QDialog):
@@ -12,29 +30,49 @@ class MainWindow(QDialog):
         self.print_tree()
         self.treeWidget.header().resizeSection(0, 300)
         self.treeWidget.header().resizeSection(1, 50)
+        self.treeWidget.itemClicked.connect(self.update_chart)
+        self.chart.setRenderHint(QPainter.Antialiasing)
+        series = QPieSeries()
 
     def print_tree(self):
         tree = DiskUsage.build_tree()
-        element = QTreeWidgetItem([tree.name])
+        element = QFileItem(tree)
         self.treeWidget.addTopLevelItem(element)
         element.setExpanded(True)
 
         def display_tree(el, catalog: DiskUsage.File):
             content = catalog.files + catalog.folders
             for item in content:
-                info = [
-                    item.name,
-                    str(item.size),
-                    item.creation_date.strftime('%H:%M:%S %d.%m.%y'),
-                    item.change_date.strftime('%H:%M:%S %d.%m.%y'),
-                    str(item.extension)
-                ]
-                tree_item = QTreeWidgetItem(info)
+                tree_item = QFileItem(item)
                 el.addChild(tree_item)
                 if item.extension == '':
                     display_tree(tree_item, item)
 
         display_tree(element, tree)
+
+    def update_chart(self):
+        file = self.treeWidget.currentItem().file
+        if file.extension != '':
+            return
+        series = QPieSeries()
+        series.setPieSize(0.5)
+        for child in file.files + file.folders:
+            if child.size > 0:
+                series.append(child.name, child.size)
+        chart = QChart()
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+        chart.addSeries(series)
+        chart.legend().hide()
+        self.chart.setChart(chart)
+        series.hovered.connect(self.on_hovered)
+
+    def on_hovered(self, slice: QPieSlice, state):
+        if state:
+            slice.setExploded(True)
+            slice.setLabelVisible()
+        else:
+            slice.setExploded(False)
+            slice.setLabelVisible(False)
 
 
 if __name__ == '__main__':
